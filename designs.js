b6d555609a6d61999b7b908da5e44772d1a86054
bmbarbre <53353333+bmbarbre@users.noreply.github.com>
2019-08-20 10:23:14 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,27 +1,26 @@
-var colpick = document.getElementById("colorPicker");
-var table = document.getElementById('pixelCanvas');
-var color = colorPicker.value;
+var colpick = document.getElementById("colorPicker"); //variab le created for the color picker element
+var table = document.getElementById('pixelCanvas');  //variable created for the pixelCanvas element
+var color = colorPicker.value  //variable created to help identify the color of the pixel/cell
 
-
-// Select color input
+// Identifies the color to enter into the cell
 var cellcolor = function (cell) {
     cell.addEventListener('click' , function() {
         event.preventDefault();
-        cell.style.backgroundColor = colorPicker.value;
+        cell.style.backgroundColor = colorPicker.value;  // changes the color based on the color selection
     })
 }
 
 
-// Select size input
+// Identifies when the size picker is submitted so that it can call function to make a grid
 var sizepick = document.getElementById("sizePicker")
 
-sizepick.addEventListener('submit', function() {
-    event.preventDefault();
+sizepick.addEventListener('submit', function() { //listens for the submit button to create the grid
     makeGrid()
 });
 
 
-// When size is submitted by the user, call makeGrid()
+// removes any existing grid, creates new grid for the pixel art, loops 
+// through to create rows and columns based on the data entered into the size picker
 function makeGrid () {
 	while (table.firstChild) {
  		table.removeChild(table.firstChild);
@@ -30,7 +29,7 @@ function makeGrid () {
         var row = table.insertRow(h)
         for (var w = 0; w<inputWidth.value; w++) {
             cell = row.insertCell(w)
-            cell.addEventListener('click',cellcolor(cell))
+            cell.addEventListener('click',cellcolor(cell)) // event listener created to identify when the color selection is clicked or changed
         }
     }
 }; 
